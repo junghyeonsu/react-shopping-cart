0c18e671da2c122ad13dd6bcd447c816a47b5e33
Molly <dngwoodo@gmail.com>
2022-02-14 10:04:27 +0900

    env: Setup mock data

---
+++ client/shopping-cart/src/fixtures/product.tsx
@@ -0,0 +1,10 @@
+const product = {
+  response: {
+    id: 1,
+    price: 10000,
+    name: '치킨',
+    imageUrl: 'http://example.com/chicken.jpg',
+  },
+};
+
+export default product;
---
+++ client/shopping-cart/src/fixtures/products.tsx
@@ -0,0 +1,18 @@
+const products = {
+  response: [
+    {
+      id: 1,
+      price: 10000,
+      name: '치킨',
+      imageUrl: 'http://example.com/chicken.jpg',
+    },
+    {
+      id: 2,
+      price: 20000,
+      name: '피자',
+      imageUrl: 'http://example.com/pizza.jpg',
+    },
+  ],
+};
+
+export default products;
