7383f1891ffe9cf2927456753b3aad0ebc4eb262
Molly <dngwoodo@gmail.com>
2022-02-24 10:04:11 +0900

    chore: vite-svgr 설치 및 적용

--- client/shopping-cart/vite.config.ts
+++ client/shopping-cart/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
+
 import react from '@vitejs/plugin-react';
+// @ts-ignore
+import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgrPlugin(),
+  ],
 });
